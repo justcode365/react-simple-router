3b953a98cb2ac62808f3a048c5cb3577f2f76795
justcode365 <justcode365@163.com>
2018-03-01 22:44:08 +0800

    update

--- src/App.js
+++ src/App.js
@@ -31,36 +31,43 @@ const Home = () => (
   </div>
 )
 
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-)
+const About = props => {
+  console.log(props)
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  )
+}
 
 const Topics = ({ match }) => (
   <div>
     <h2>Topics</h2>
     <ul>
       <li>
-        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+        <Link to={`/topics/rendering`}>Rendering with React</Link>
       </li>
       <li>
-        <Link to={`${match.url}/components`}>Components</Link>
+        <Link to={`/topics/components`}>Components</Link>
       </li>
       <li>
-        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+        <Link to={`/topics/props-v-state`}>Props v. State</Link>
       </li>
     </ul>
 
-    <Route path={`${match.url}/:topicId`} component={Topic} />
-    <Route exact path={match.url} render={() => <h3>Please select a topic.</h3>} />
+    <Route path={`/topics/:topicId`} component={Topic} />
+    {/* <Route exact path={match.url} render={() => <h3>Please select a topic.</h3>} /> */}
   </div>
 )
 
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-)
+const Topic = ({ match }) => {
+  console.log(match)
+  return (
+    <div>
+      1
+      {/* <h3>{match.params.topicId}</h3> */}
+    </div>
+  )
+}
 
 export default BasicExample
--- src/package/react-simple-router/Route.js
+++ src/package/react-simple-router/Route.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react'
 import { Consumer } from './Router'
+import pathToRegexp from 'path-to-regexp'
 
 class Route extends Component {
-  match = (path, router) => {
-    return path === router
+  match = (path, url) => {
+    return true
+  }
+
+  paramsMatch = (path, url) => {
+    let keys = []
+    let re = pathToRegexp(path, keys)
+    const match = re.exec(url)
+
+    if (!match) return {}
+
+    const [_url, ...values] = match
+
+    console.log(keys, values)
+    return keys.reduce((temp, key, index) => {
+      temp[key.name] = values[index]
+      return temp
+    }, {})
   }
 
   render() {
-    const { path, component, router } = this.props
+    const { path, component, url } = this.props
+    const params = this.paramsMatch(path, url)
+    const props = { match: { path, url, params } }
 
-    return (
-      <Consumer>
-        {({ router }) => (this.match(path, router) ? React.createElement(component) : null)}
-      </Consumer>
-    )
+    return this.match(path, url) ? React.createElement(component, props) : null
   }
 }
 
-export default Route
+export default props => <Consumer>{({ router }) => <Route {...props} url={router} />}</Consumer>
